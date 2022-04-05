674c97f99b2f7ebb3fe8a0165f9dacdf3cded780
jbiehl88 <jcbiehl8@gmail.com>
2022-04-05 11:14:56 -0500

    lunch

--- db/index.js
+++ db/index.js
@@ -1,1 +1,9 @@
 // require and re-export all files in this db directory (users, activities...)
+
+const { client } = require("./db/client");
+const { rebuildDB } = require("./db/seedData");
+
+module.exports = {
+  client,
+  rebuildDB,
+};
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // create the express server here
+require("dotenv").config();
+const { PORT = 3000 } = process.env;
+const express = require("express");
+const server = express();
+const apiRouter = require("./api");
+const { client } = require("./db");
+
+client.connect();
