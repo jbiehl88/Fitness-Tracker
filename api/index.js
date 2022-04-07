f243af7545f3dbc9ce07bbd656ba73526b3ce2d3
jbiehl88 <jcbiehl8@gmail.com>
2022-04-07 14:26:23 -0500

    start health

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
+
+const express = require("express");
+const apiRouter = express.Router();
+
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    message: error.message,
+  });
+});
+
+module.exports = apiRouter;
--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const express = require("express");
 const server = express();
 const apiRouter = require("./api");
 const { client } = require("./db/client");
+const cors = require("cors");
 
 client.connect();
+
+server.use(cors());
 
 server.use(express.json());
 
